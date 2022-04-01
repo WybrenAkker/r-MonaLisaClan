790f93138139014ac3e85f226c9533678f918a3d
Walter Hulsebos <77513543+Walter-Hulsebos@users.noreply.github.com>
2022-04-01 23:35:40 +0200

    Update Image Whitespace

--- alliance_overlay.user.js
+++ alliance_overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         April Fools
 // @namespace    http://tampermonkey.net/
-// @version      0.8
+// @version      0.9
 // @description  Keep the canvas beautiful!
 // @author       /u/djdogjuam2 & /u/irate_kalypso
 // @match        https://hot-potato.reddit.com/embed*
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://i.imgur.com/No2pycV.png";
+            i.src = "https://i.imgur.com/nyLqBaO.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
