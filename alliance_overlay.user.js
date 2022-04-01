38ef1ace39a563a089f4fe1a95529f43c537b394
Walter Hulsebos <77513543+Walter-Hulsebos@users.noreply.github.com>
2022-04-01 23:30:48 +0200

    Updated updateURL

--- alliance_overlay.user.js
+++ alliance_overlay.user.js
@@ -3,11 +3,11 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.7
 // @description  Keep the canvas beautiful!
-// @author       /u/irate_kalypso
+// @author       /u/djdogjuam2 & /u/irate_kalypso
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/iratekalypso/r-place-2022/raw/main/alliance_overlay.user.js
-// @downloadURL  https://github.com/iratekalypso/r-place-2022/raw/main/alliance_overlay.user.js
+// @updateURL    https://github.com/Walter-Hulsebos/r-MonaLisaClan/blob/main/alliance_overlay.user.js
+// @downloadURL  https://github.com/Walter-Hulsebos/r-MonaLisaClan/raw/main/alliance_overlay.user.js
 // @grant        none
 // ==/UserScript==
 if (window.top !== window.self) {
