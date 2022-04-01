587bb66df22c0e8a9ccc7cc26a64c2bf7945a4d2
Wybren van den Akker <wybren.akker@gmail.com>
2022-04-02 00:06:27 +0200

    second attempt at making toggle button

--- alliance_overlay.user.js
+++ alliance_overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         April Fools
+// @name         MonaLisaClan
 // @namespace    http://tampermonkey.net/
-// @version      0.20
+// @version      0.30
 // @description  Keep the canvas beautiful!
 // @author       /u/djdogjuam2 & /u/irate_kalypso
 // @match        https://hot-potato.reddit.com/embed*
@@ -14,18 +14,36 @@ if (window.top !== window.self) {
     window.addEventListener('load', () => {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
-            const i = document.createElement("img");
+            const i = document.createElement("overlay_img");
             i.src = "https://i.imgur.com/tiIMTXK.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
-            
-            const cb = document.querySelector(".mona-lisa-zoom-button");
-            const c2 = toHtml(`<mona-lisa-icon-button id="place-template-button" class="visible" style="display:inline-block; top:-110px; background-image:inherit">T</mona-lisa-icon-button>`);
-            cb.parentNode.insertBefore(c2, cb);
             
             return i;
         })())
 
     }, false);
-
 }
+let active = 1;
+
+document.addEventListener("keypress", function(e) {
+    if (e.which == 84) {
+      const img = document.querySelector("overlay_img");
+         if(active == 1) 
+         {
+            active = 2;
+            img.style.opacity = 0.9;
+        } else if (active == 2) 
+        {
+            active = 0;
+            img.style.visibility = "hidden";
+        } 
+        else if (active == 0) 
+        {
+            active = 1;
+            img.style.visibility = "inherit";
+            img.style.opacity = 0.5;
+        }
+    }
+  });
+
