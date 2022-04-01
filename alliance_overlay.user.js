3880e8d9c0adfa4d4e45907022d753f1cca4e675
Wybren van den Akker <wybren.akker@gmail.com>
2022-04-02 00:09:08 +0200

    Update alliance_overlay.user.js

--- alliance_overlay.user.js
+++ alliance_overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MonaLisaClan
 // @namespace    http://tampermonkey.net/
-// @version      0.30
+// @version      0.40
 // @description  Keep the canvas beautiful!
 // @author       /u/djdogjuam2 & /u/irate_kalypso
 // @match        https://hot-potato.reddit.com/embed*
